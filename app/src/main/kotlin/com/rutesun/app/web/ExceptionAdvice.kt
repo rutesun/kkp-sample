d802a82e7b4e5d23132b6b0cbae6b1d5dd853373
rutesun <rutesun@gmail.com>
2020-11-23 01:50:36 +0900

    + ExceptionAdvisor

---
+++ app/src/main/kotlin/com/rutesun/app/web/ExceptionAdvice.kt
@@ -0,0 +1,73 @@
+package com.rutesun.app.web
+
+import com.rutesun.core.domain.AlreadyReceivedException
+import com.rutesun.core.domain.ExpiredDistributionException
+import com.rutesun.core.domain.NotJoinedUser
+import com.rutesun.core.domain.NotOwnerException
+import com.rutesun.core.service.MoneyExhaustedException
+import com.rutesun.core.service.NotFoundException
+import com.rutesun.core.service.NotFoundUserException
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class ExceptionAdvice {
+    private val log = LoggerFactory.getLogger(this.javaClass)
+
+    @ExceptionHandler(value = [ExpiredDistributionException::class])
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    fun handleExpiredDistributionException(ex: ExpiredDistributionException): ResponseEntity<*> {
+        val message = "${ex.distribution.id}, ${ex.message}"
+        log.error(message)
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message)
+    }
+
+    @ExceptionHandler(value = [AlreadyReceivedException::class])
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    fun handleAlreadyReceivedException(ex: AlreadyReceivedException): ResponseEntity<*> {
+        val message = "${ex.item.id}, ${ex.message}"
+        log.error(message)
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(message)
+    }
+
+    @ExceptionHandler(value = [NotJoinedUser::class])
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    fun handleNotJoinedUser(ex: NotJoinedUser): ResponseEntity<*> {
+        val message = "${ex.user.id}, ${ex.message}"
+        log.error(message)
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(message)
+    }
+
+    @ExceptionHandler(value = [NotOwnerException::class])
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    fun handleNotOwnerException(ex: NotOwnerException): ResponseEntity<*> {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ex.message)
+    }
+
+    @ExceptionHandler(value = [NotFoundException::class])
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    fun handleNotFoundException(ex: NotFoundException): ResponseEntity<*> {
+        log.error(ex.message)
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.message)
+    }
+
+    @ExceptionHandler(value = [NotFoundUserException::class])
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    fun handleNotFoundUserException(ex: NotFoundUserException): ResponseEntity<*> {
+        val message = "${ex.userId}, ${ex.message}"
+        log.error(message)
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.message)
+    }
+
+    @ExceptionHandler(value = [MoneyExhaustedException::class])
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    fun handleMoneyExhaustedException(ex: MoneyExhaustedException): ResponseEntity<*> {
+        val message = "${ex.moneyDistribution.id}, ${ex.message}"
+        log.error(message)
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.message)
+    }
+}
