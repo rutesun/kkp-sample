18a6be099200e3cbb160a5907b99555beb5eb13d
rutesun <rutesun@gmail.com>
2020-11-28 16:49:11 +0900

    compile 타임에 체크할 수 있는 Query 를 사용하자

--- core/service/src/main/kotlin/com/rutesun/core/repository/MoneyDistributionRepository.kt
+++ core/service/src/main/kotlin/com/rutesun/core/repository/MoneyDistributionRepository.kt
@@ -2,13 +2,13 @@ package com.rutesun.core.repository
 
 import com.rutesun.core.domain.MoneyDistribution
 import com.rutesun.core.domain.Token
-import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface MoneyDistributionRepository : JpaRepository<MoneyDistribution, Long> {
     fun existsByToken(token: Token): Boolean
     fun findByToken(token: Token): MoneyDistribution?
 
-    @EntityGraph(value = "items", type = EntityGraph.EntityGraphType.LOAD)
+    @Query("SELECT md FROM MoneyDistribution md INNER JOIN FETCH md.items WHERE md.token = ?1")
     fun findWithItemsByToken(token: Token): MoneyDistribution?
 }
